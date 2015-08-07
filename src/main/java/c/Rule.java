c59891935320461d49c8afe07d930a8f7442fa0a
denny <andrden@gmail.com>
2015-08-07 19:42:03 +0300

    parsing

--- src/main/java/c/Rule.java
+++ src/main/java/c/Rule.java
@@ -2,6 +2,7 @@ package c;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created by denny on 8/6/15.
@@ -11,6 +12,18 @@ public class Rule {
 
     public Rule(List<String> lines) {
         this.lines = new ArrayList<>(lines);
+        for( String l : lines ){
+            System.out.println(splitLine(l));
+        }
+    }
+
+    List<String> splitLine(String line){
+        StringTokenizer st = new StringTokenizer(line, " \t");
+        List<String> ret = new ArrayList<>();
+        while(st.hasMoreTokens()){
+            ret.add(st.nextToken());
+        }
+        return ret;
     }
 
     @Override
