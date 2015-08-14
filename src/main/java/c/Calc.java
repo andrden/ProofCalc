1524613e114e260a624b8f46df421400c6e78e5c
denny <andrden@gmail.com>
2015-08-14 18:20:02 +0300

    cos( pi / 2 ) = 0 by recursive substitution

--- src/main/java/c/Calc.java
+++ src/main/java/c/Calc.java
@@ -18,22 +18,47 @@ public class Calc {
 
         Expr expr = q.assertion;
         for(;;) {
-            boolean changed = false;
-            for (Rule r : rules) {
-                if (r.assertion.node.equals("=")) {
-                    Map<String, Expr> unifMap = r.assertion.sub.get(0).unify(expr);
-                    if( unifMap!=null ) {
-                        System.out.println("unify with " + r + " results in " + unifMap);
-                        expr = r.assertion.sub.get(1).substitute(unifMap);
-                        System.out.println("simplified: " + expr);
-                        changed = true;
-                        break;
-                    }
-                }
-            }
-            if( ! changed ){
+            Expr exprNew = exprSimplifyDeep(expr);
+            if( exprNew==null ){
                 break;
+            }else{
+                expr = exprNew;
+                System.out.println("QUEST res: "+expr);
             }
         }
+    }
+
+    Expr exprSimplifyDeep(Expr expr) {
+        //System.out.println("Try simplify: "+expr);
+        Expr enew = exprSimplify(expr);
+        if( enew!=null ){
+            return enew;
+        }
+        if( expr.sub==null ){
+            return null;
+        }
+        for( int i=0; i<expr.sub.size(); i++ ){
+            Expr e = exprSimplifyDeep(expr.sub.get(i));
+            if( e!=null ){
+                expr.sub.set(i, e);
+                return expr;
+            }
+        }
+        return null;
+    }
+
+    Expr exprSimplify(Expr expr) {
+        for (Rule r : rules) {
+            if (r.assertion.node.equals("=")) {
+                Map<String, Expr> unifMap = r.assertion.sub.get(0).unify(expr);
+                if( unifMap!=null ) {
+                    //System.out.println("unify with " + r + " results in " + unifMap);
+                    Expr exprNew = r.assertion.sub.get(1).substitute(unifMap);
+                    //System.out.println(expr + " ==simplified==> " + exprNew);
+                    return exprNew;
+                }
+            }
+        }
+        return null;
     }
 }
