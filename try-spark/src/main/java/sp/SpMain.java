84b58813e65054c1b536cb76fbb0c0c8382c5351
denny <andrden@gmail.com>
2015-11-16 16:37:20 +0200

    lim0 introduced

--- try-spark/src/main/java/sp/SpMain.java
+++ try-spark/src/main/java/sp/SpMain.java
@@ -1,7 +1,55 @@
 package sp;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+[root@as114 ~]# HADOOP_USER_NAME=hdfs spark-shell --master yarn
+
+scala> sqlContext.read.parquet("/adslogs/epommarket/1/impressions").registerTempTable("imp")
+scala> sqlContext.sql("SELECT browser,count(*) from imp where day='2015-11-09' group by browser").collect().foreach(println(_))
+
+ */
 public class SpMain{
     public static void main(String[] args) {
         System.out.println("try-spark module");
+
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("yarn-cluster")
+                .setAppName("JavaSparkPi");
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+        int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+        int n = 100000 * slices;
+        List<Integer> l = new ArrayList<Integer>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+
+        JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+        int count = dataSet.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                double x = Math.random() * 2 - 1;
+                double y = Math.random() * 2 - 1;
+                return (x * x + y * y < 1) ? 1 : 0;
+            }
+        }).reduce(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) {
+                return integer + integer2;
+            }
+        });
+
+        System.out.println("Pi is roughly " + 4.0 * count / n);
+
+        jsc.stop();
     }
 }
